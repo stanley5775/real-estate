9e278eb96bcf7d70fa061ccd99b5ad64a4c4f550
stanley5775 <stanleyibekwe21@gmail.com>
2025-10-08 07:44:08 +1300

    fix missing space before span tag

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -42,7 +42,7 @@ const Contact = () => {
       id="Contact"
     >
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
-        Contact
+        Contact{" "}
         <span className="underline underline-offset-4 decoration-1 under font-light">
           With Us
         </span>
